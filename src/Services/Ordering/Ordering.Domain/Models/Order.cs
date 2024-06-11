17db6cc8a8f64678acb721d6d6a868df74cf1ec7
Eduardo Alvarez <eduardo.alvarez@tecso.coop>
2024-06-11 18:04:02 -0300

    Section 14 - Complete - Tactical DDD (Entities, Value Objects, Aggregates, Aggregate Roots), Primitive Obsession and Strongly IDs, Anemic-domain vs Rich-domain model entity, Domain Events vs Integration Events.

---
+++ src/Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -0,0 +1,67 @@
+﻿namespace Ordering.Domain.Models;
+
+public class Order : Aggregate<OrderId>
+{
+    private readonly List<OrderItem> _orderItems = [];
+
+    public IReadOnlyList<OrderItem> OrderItems => _orderItems.AsReadOnly();
+
+    public CustomerId CustomerId { get; private set; } = default!;
+
+    public OrderName OrderName { get; private set; } = default!;
+
+    public Address ShippingAddress { get; private set; } = default!;
+
+    public Address BillingAddress { get; private set; } = default!;
+
+    public Payment Payment { get; private set; } = default!;
+
+    public OrderStatus Status { get; private set; } = OrderStatus.Pending;
+
+    public decimal TotalPrice
+    {
+        get => OrderItems.Sum(oi => oi.Price * oi.Quantity);
+        private set { }
+    }
+
+    public static Order Create(OrderId id, CustomerId customerId, OrderName orderName, Address shippingAddress, Address billingAddress, Payment payment)
+    {
+        var order = new Order
+        {
+            Id = id,
+            CustomerId = customerId,
+            OrderName = orderName,
+            ShippingAddress = shippingAddress,
+            BillingAddress = billingAddress,
+            Payment = payment,
+            Status = OrderStatus.Pending,
+        };
+        order.AddDomainEvent(new OrderCreatedEvent(order));
+        return order;
+    }
+
+    public void Update(OrderName orderName, Address shippingAddress, Address billingAddress, Payment payment, OrderStatus status)
+    {
+        OrderName = orderName;
+        ShippingAddress = shippingAddress;
+        BillingAddress = billingAddress;
+        Payment = payment;
+        Status = status;
+        AddDomainEvent(new OrderUpdatedEvent(this));
+    }
+
+    public void AddItem(ProductId productId, int quantity, decimal price)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(quantity);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+        var orderItem = new OrderItem(Id, productId, quantity, price);
+        _orderItems.Add(orderItem);
+    }
+
+    public void RemoveItem(ProductId productId)
+    {
+        var orderItem = _orderItems.FirstOrDefault(oi => oi.ProductId == productId);
+        if (orderItem is null) return;
+        _orderItems.Remove(orderItem);
+    }
+}
