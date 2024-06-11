8fb2664f7241e6951154c6b9908d2b0efcd76553
Eduardo Alvarez <eduardo.alvarez@tecso.coop>
2024-06-10 22:30:44 -0300

    Section 12 - Complete - Consuming Discount gRPC service from Basket Microservice when Adding Cart Item

--- src/Services/Basket/Basket.API/Basket/Commands/StoreBasket/StoreBasketCommandHandler.cs
+++ src/Services/Basket/Basket.API/Basket/Commands/StoreBasket/StoreBasketCommandHandler.cs
@@ -1,11 +1,28 @@
-﻿namespace Basket.API.Basket.Commands.StoreBasket;
+﻿using Discount.gRPC;
+
+namespace Basket.API.Basket.Commands.StoreBasket;
 
 public class StoreBasketCommandHandler(
-    IBasketRepository repository) : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+    IBasketRepository repository,
+    DiscountProtoService.DiscountProtoServiceClient discountProto) : ICommandHandler<StoreBasketCommand, StoreBasketResult>
 {
     public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
     {
+        await ApplyDiscount(command.Cart, cancellationToken);
+
+        // Store basket in database (use Marte upsert - if exist = update, if not exist = insert) and Update Cache
         await repository.StoreBasket(command.Cart, cancellationToken);
+
         return new StoreBasketResult(command.Cart.UserName);
+    }
+
+    private async Task ApplyDiscount(ShoppingCart cart, CancellationToken cancellationToken)
+    {
+        // Communicate with Discount.gRPC and calculate latest prices of products into sc
+        foreach (var item in cart.Items)
+        {
+            var coupon = await discountProto.GetDiscountAsync(new GetDiscountRequest { ProductName = item.ProductName }, cancellationToken: cancellationToken);
+            item.Price -= coupon.Amount;
+        }
     }
 }
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,3 +1,4 @@
+using Discount.gRPC;
 using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
@@ -30,6 +31,18 @@ builder.Services.AddStackExchangeRedisCache(options =>
     options.Configuration = builder.Configuration.GetConnectionString("Redis");
     //options.InstanceName = "Basket";
 });
+builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(options =>
+{
+    options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]!);
+})
+    .ConfigurePrimaryHttpMessageHandler(() =>
+    {
+        var handler = new HttpClientHandler
+        {
+            ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator,
+        };
+        return handler;
+    });
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 builder.Services.AddHealthChecks()
     .AddNpgSql(builder.Configuration.GetConnectionString("Database")!)
