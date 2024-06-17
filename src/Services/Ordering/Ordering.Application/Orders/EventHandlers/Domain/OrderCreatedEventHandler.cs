1026456ab5c55f51c1a9cac9ba8070b7a147bf15
Eduardo Alvarez <eduardo.alvarez@tecso.coop>
2024-06-17 11:42:52 -0300

    Section 18 - BasketCheckoutEventHandler created, Dual-write problem and Outbox pattern, Subscriber and Consumer with MassTransit and RabbitMQ, Domain and Integration Event flow, Feature Management.

---
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
@@ -0,0 +1,19 @@
+﻿using MassTransit;
+using Microsoft.FeatureManagement;
+
+namespace Ordering.Application.Orders.EventHandlers.Domain;
+
+public class OrderCreatedEventHandler(
+    ILogger<OrderCreatedEventHandler> logger,
+    IPublishEndpoint publishEndpoint,
+    IFeatureManager featureManager) : INotificationHandler<OrderCreatedEvent>
+{
+    public async Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Domain Event handled: {DomainEvent}", domainEvent.GetType().Name);
+        var featureIsEnabled = await featureManager.IsEnabledAsync("OrderFullfilment");
+        if (!featureIsEnabled) return;
+        var orderCreatedIntegrationEvent = domainEvent.Order.ToOrderDto();
+        await publishEndpoint.Publish(orderCreatedIntegrationEvent, cancellationToken);
+    }
+}
